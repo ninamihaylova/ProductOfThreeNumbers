0feba3ae1358a2f0d267282a761a0405d288f791
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-16 16:30:32 +0300

    Initial commit

---
+++ ProductOfThreeNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+class ProductOfThreeNumbers{
+    public static void Main(string[] args)
+    {
+        double number1 = double.Parse(Console.ReadLine());
+        double number2 = double.Parse(Console.ReadLine());
+        double number3 = double.Parse(Console.ReadLine());
+        double product = number1 * number2 * number3;
+
+                if (product > 0)
+                {
+                    Console.WriteLine("positive");
+                }
+                else if (product < 0)
+                {
+                    Console.WriteLine("negative");
+                }
+                else
+                {
+                    Console.WriteLine("zero");
+                }
+    }
+        }
